f2c60459a616e5cb9e9a0d9e4de1a497d737f9d3
Diego_dos_Santos_Fernandes <55159614+DIEG0FERNANDES@users.noreply.github.com>
2023-10-30 01:38:12 -0400

    Initial commit
    Correção de erros bobos feito por conta de desespero apenas

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,16 +1,15 @@
 import { Router } from 'express'
-// import { getRepository } from 'typeorm'
 import { ProductController } from '../controllers/ProductController';
 import { Product } from '../entities/Product'
 
 export const productsRouter = Router();
 const productCtrl = new ProductController
 
+const errorMessages: string[] = []
 productsRouter.post('/products', async (req, res) => {
     try {
         const { description, price, quantity } = req.body;
 
-        const errorMessages: string[] = []
 
         if (!description || !price || !quantity) {
             return res.status(400).json({ message: 'Invalid inputs' });
@@ -25,15 +24,6 @@ productsRouter.post('/products', async (req, res) => {
             return res.status(201).json({ task: savedProduct, message: 'Product registered' })
         }
 
-        // const product = new Product();
-        // product.description = description;
-        // product.price = price;
-        // product.quantity = quantity;
-
-        // const productRepository = getRepository(Product);
-        // await productRepository.save(product);
-
-        // return res.status(201).json({message: 'Product registered' });
     } catch (error) {
         console.error(error);
         return res.status(500).json({ message: 'Internal server error' });
@@ -43,15 +33,15 @@ productsRouter.post('/products', async (req, res) => {
 productsRouter.get('/products/:id', async (req, res) => {
     try {
         const id = parseInt(req.params.id);
-        // const productRepository = getRepository(Product);
-        // const product = await productRepository.findOne({ where: { id } });
         const product = await productCtrl.getProductById(id);
 
         if (!product) {
             return res.status(404).json({ message: 'Product not found' });
         }
 
-        return res.status(200).json({ product });
+        if (errorMessages.length === 0) {
+            return res.status(200).json({ product });
+        }
     } catch (error) {
         console.error(error);
         return res.status(500).json({ message: 'Internal server error' });
@@ -61,11 +51,11 @@ productsRouter.get('/products/:id', async (req, res) => {
 productsRouter.get('/products/description/:description', async (req, res) => {
     try {
         const description = req.params.description;
-        // const productRepository = getRepository(Product);
-        // const products = await productRepository.find({ where: { description } });
         const products = await productCtrl.getProductsByDescription(description);
 
-        return res.status(200).json({ products });
+        if (errorMessages.length === 0) {
+            return res.status(200).json({ products });
+        }
     } catch (error) {
         return res.status(500).json({ message: 'Internal server error' });
     }
